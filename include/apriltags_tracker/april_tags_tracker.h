de601c6ebd97ef0738a5b5203d14979304eef2c9
walter <dqyi11@gmail.com>
2016-01-13 01:39:22 -0700

    new files

--- include/apriltags_tracker/april_tags_tracker.h
+++ include/apriltags_tracker/april_tags_tracker.h
@@ -3,10 +3,17 @@
 
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
+#include <AprilTags/TagDetector.h>
+#include <AprilTags/Tag16h5.h>
+#include <AprilTags/Tag25h7.h>
+#include <AprilTags/Tag25h9.h>
+#include <AprilTags/Tag36h9.h>
+#include <AprilTags/Tag36h11.h>
+
 
 class AprilTagsTracker {
 public:
-  AprilTagsTracker();
+  AprilTagsTracker( AprilTags::TagCodes codes = AprilTags::tagCodes36h11 );
   virtual ~AprilTagsTracker();
 
   void imageCallback( const sensor_msgs::ImageConstPtr& msg);
@@ -14,6 +21,9 @@ public:
   ros::NodeHandle m_nh;
   image_transport::ImageTransport m_it;
   image_transport::Subscriber m_sub;
+
+  AprilTags::TagDetector* m_tag_detector;
+  AprilTags::TagCodes     m_tag_codes;
 };
 
 #endif // APRIL_TAGS_TRACKER_H_
--- src/april_tags_tracker.cpp
+++ src/april_tags_tracker.cpp
@@ -2,6 +2,7 @@
 #include <opencv2/opencv.hpp>
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/image_encodings.h>
+
 #include "apriltags_tracker/april_tags_tracker.h"
 
 #define APRIL_TAGS_TRACKER_VIEW "April Tags Tracker"
@@ -10,7 +11,7 @@
 void AprilTagsTracker::imageCallback( const sensor_msgs::ImageConstPtr& msg) {
   cv_bridge::CvImagePtr cv_ptr;
   try {
-    cv_ptr = cv_bridge::toCvCopy( msg, "mono8" ); 
+    cv_ptr = cv_bridge::toCvCopy( msg, "bgr8" ); 
   }
   catch( cv_bridge::Exception& e ) {
     ROS_ERROR( "cv_bridge exce[topm: %s", e.what() );
@@ -20,7 +21,7 @@ void AprilTagsTracker::imageCallback( const sensor_msgs::ImageConstPtr& msg) {
   cv::waitKey(30);
 }
 
-AprilTagsTracker::AprilTagsTracker() : m_it( m_nh )  {
+AprilTagsTracker::AprilTagsTracker( AprilTags::TagCodes codes  ) : m_it( m_nh ) , m_tag_codes( codes )  {
   cv::namedWindow(APRIL_TAGS_TRACKER_VIEW);
   cv::startWindowThread();
   
